fd62f161ae51b9b0eab5d1846f40c3f8799b8cdb
Dan Ferguson <daniefer@icloud.com>
2018-12-27 19:58:29 -0600

    initial commit

---
+++ App/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ACA_QuickStart_Console
+{
+	public class HelloWorldController
+	{
+		public string Name { get; private set; }
+
+		public void PrintHelloWorld()
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public void AskForName()
+		{
+			Console.WriteLine("What is your name?");
+			Name = Console.ReadLine();
+		}
+
+		public void PrintHello()
+		{
+			Console.WriteLine($"Hello {Name}");
+		}
+	}
+}
---
+++ App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ACA_QuickStart_Console
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			// Print hello, get the users name, then say hello to them using the HelloWorldController
+		}
+	}
+}
